6177ffa2ec60666f8ad6b8c1eb14cc7a0cc892a0
Tomasz Oszczepalski <tomasz.oszczepalski@tv2.no>
2025-04-30 20:25:37 +0200

    code block text

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post("/slack/command", async (req, res) => {
 		// Respond to Slack
 		await axios.post(response_url, {
 			response_type: "ephemeral",
-			text: `💡 ${rephrased}`
+			text: `💡 *Rephrased:*\n\`\`\`\n${rephrased}\n\`\`\``
 		});
 
 		res.status(200).end(); // Ack the slash command quickly
